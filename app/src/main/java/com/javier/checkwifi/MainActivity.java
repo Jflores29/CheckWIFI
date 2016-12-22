90c033c267a3d573fe99c1a3ae086b13b650abe9
Javier <jflores71814@yahoo.com>
2016-12-22 15:28:08 -0500

    first commit

---
+++ app/src/main/java/com/javier/checkwifi/MainActivity.java
@@ -0,0 +1,43 @@
+package com.javier.checkwifi;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView textView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = (TextView) findViewById(R.id.main_tv);
+
+
+    }
+
+
+    public void checkMagic(View view)
+    {
+        if (AmIConnected()== true){
+            textView.setText("You are connected");
+        }
+        else
+        {
+            textView.setText("Something went wrong!");
+        }
+    }
+
+
+    public boolean AmIConnected(){
+            ConnectivityManager connectivityManager
+                    = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+            return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+        }
+    }
